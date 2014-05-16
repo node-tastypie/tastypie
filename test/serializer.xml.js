31fa374d72f128eed0344abf9c26619e9a10f822
Eric Satterwhite <esatterwhite@wi.rr.com>
2014-05-16 18:55:24 -0500

    initial pass at xml serialization

--- test/serializer.xml.js
+++ test/serializer.xml.js
@@ -1,7 +1,18 @@
 var assert = require("assert")
+   ,serializer = require("../lib/serializer")
 
 describe('serializer#xml', function(){
 	it("should do some things", function(){
-		assert.equal(true, true)
+		var s = new serializer();
+		console.log( s.serialize({key:'value', foo:{alt:{test:1}}}, 'application/xml') )
+	})
+
+	it('should do other things', function(){
+		var s = new serializer({
+			xmlattr:true
+		})
+
+		console.log( s.serialize({key:'value', foo:{alt:{test:1}}}, 'application/xml') )
+
 	})
 })
