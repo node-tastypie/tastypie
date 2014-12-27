5b0c99c279d34befd5798f89d63458d711e5ac8f
Eric Satterwhtie <esatterwhite@wi.rr.com>
2014-12-27 16:37:37 -0600

    [PIE-4] Adding some more documentation
    mode method docs for Resource

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+/*jshint laxcomma: true, smarttabs: true*/
+/*globals module,process,require,exports,__dirname,__filename */
+'use strict';
+/**
+ * Resource based REST Framework built ontop of hapi.js
+ * @module tastypie
+ * @author Eric Satterwhite
+ * @since 0.0.1
+ * @requires tastypie/lb
+ */
+
 module.exports = require('./lib')
