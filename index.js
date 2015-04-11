252e39fab7161a7df74910bb3bb06d0fa9f376f3
Eric Satterwhtie <esatterwhite@wi.rr.com>
2015-04-11 17:09:57 -0500

    adding jsdoc

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /*jshint laxcomma: true, smarttabs: true*/
-/*globals module,process,require,exports,__dirname,__filename */
 'use strict';
 /**
  * Resource based REST Framework built ontop of hapi.js
