b2391fc8a5dfecd95a47635311c852307b6864ee
Eric Satterwhite <esatterwhite@wi.rr.com>
2015-06-11 16:33:16 -0500

    Bad filter errors should return a 400 status

--- test/resource.mongoose.spec.js
+++ test/resource.mongoose.spec.js
@@ -62,7 +62,7 @@ describe('MongoResource', function( ){
 		})
 	})
 
-	describe('queryset', function( ){
+	describe('filtering', function( ){
 
 		var queryset, Mongo;
 		before(function( done ){
@@ -72,28 +72,59 @@ describe('MongoResource', function( ){
 				options:{
 
 					queryset: queryset
+					,filtering:{
+						name:1,
+						age:['lt', 'lte']
+					}
 				}
 				,fields:{
-					name:{type:'char', attribute:'name.first'}
+					name:{type:'char', attribute:'name.first'},
+					age:{type:'int'},
+					eyes:{type:'char', attribute:'eyeColor'}
 				}
 			});
+
 			mongo.use('test', new Mongo );
 			done();
 		})
 
-		it('should do things', function( done ){
+		it('should respect the limit param', function( done ){
 			server.inject({
-				url:'/api/mongo/test?limit=25'
+				url:'/api/mongo/test?limit=10'
 				,method:'get'
 				,headers:{
 					Accept:'application/json'
 				}
 			},function( response ){
 				var data = JSON.parse( response.result );
-				data.data.length.should.equal( 25 );
+				data.data.length.should.equal( 10 );
 				done();
 			})
+		})
 
+		it('should respect filtering definition', function( done ){
+			server.inject({
+				url:'/api/mongo/test?name__istartswith=c&age__lt=100'
+				,method:'get'
+				,headers:{
+					Accept:'application/json'
+				}
+			},function( response ){
+				done()
+			})
+		})
+
+		it('should return  filters', function( done ){
+			server.inject({
+				url:'/api/mongo/test?name__istartswith=c&age__gt=100'
+				,method:'get'
+				,headers:{
+					Accept:'test/json'
+				}
+			},function( response ){
+				assert.equal(response.statusCode, 400)
+				done()
+			})
 		})
 	})
 })
