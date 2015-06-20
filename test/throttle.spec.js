b5f5c46562cd5dd3e7e8732d8fa0de7dd1076316
Eric Satterwhite <esatterwhite@wi.rr.com>
2015-06-20 16:32:39 -0500

    Throttle tests.
    verify changing the settings yeilds the expected behavior

---
+++ test/throttle.spec.js
@@ -0,0 +1,74 @@
+var assert = require('assert')
+  , should = require('should')
+  , Throttle = require('../lib/throttle')
+
+  describe("Throttle", function(){
+  	describe("Memory", function(){
+
+  		var short, long;
+
+  		before(function( done ){
+  			short = new Throttle.Memory({
+  				at:5
+  				,timeframe:1000
+  			});
+
+  			long = new Throttle.Memory({
+  				at:10
+  				,timeframe: 2000
+  			});
+
+  			done()
+  		});
+
+
+
+  		it('should respect', function( done ){
+  			this.timeout(2000)
+  			var throttle, key;
+  			key = "SHORT"
+  			for(var count = 0; count < 4; count++){
+	  			short.incr( key );
+	  			throttle = short.toThrottle( key );
+	  			// console.log( throttle )
+  				throttle.should.be.false
+  			}
+
+			short.incr( key );
+			throttle = short.toThrottle( key );
+			throttle.should.be.true
+
+			setTimeout(function(){
+				short.incr( key );
+				throttle = short.toThrottle( key );
+				throttle.should.be.false
+	  			done();
+			}, 1100 )
+
+  		});
+
+  		it('should respect long time frames', function( done ){
+  			this.timeout(3000)
+  			var throttle, key;
+  			key = "LONG"
+  			for(var count = 0; count < 9; count++){
+	  			long.incr( key );
+	  			throttle = long.toThrottle( key );
+	  			// console.log( throttle )
+  				throttle.should.be.false
+  			}
+
+			long.incr( key );
+			throttle = long.toThrottle( key );
+			throttle.should.be.true
+
+			setTimeout(function(){
+				long.incr( key );
+				throttle = long.toThrottle( key );
+				throttle.should.be.false
+	  			done();
+			}, 2100 )
+
+  		})
+  	})
+  })
