e40ae04fa14efa0e0475fa402389d485af60da6b
Eric Satterwhite <esatterwhite@wi.rr.com>
2015-07-27 18:58:31 -0500

    [NOISSUE] syntax clean ups.
    == -> === where possible

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var child_process = require('child_process')
   , fs = require('fs')
   , util = require("util")
-  , production = (process.env.NODE_ENV == 'test')
+  , production = (process.env.NODE_ENV === 'test')
   , html
   , coverage
   , mocha
