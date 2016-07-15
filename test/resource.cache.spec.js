3465ec7d7576683c076da99fb871d53040d0d62f
Eric Satterwhite <esatterwhite@wi.rr.com>
2016-07-15 14:09:03 -0500

    update cache tests with new cache definition style

--- test/resource.cache.spec.js
+++ test/resource.cache.spec.js
@@ -1,32 +1,36 @@
 var async = require('async')
   , hapi = require('hapi')
+  , sinon = require('sinon')
   , Api = require('../lib/api')
   , Resource = require("../lib/resource")
   , Cache = require('../lib/cache')
   ;
 
 describe('resource', function(){
-	var resource, server, c;
+	var resource, server, c, instance;
 	before( function( done ){
 		server = new hapi.Server({});
 		server.connection({host:'localhost'})
 		resource = Resource.extend({
-
-			get_object:function( bundle, callback ){
+			options:{
+				cache: {
+					engine: 'catbox-memory'
+				}
+				
+			}
+			,get_object:function( bundle, callback ){
 				callback(null, { key:new Date() } )
 			}
 		});
-		c =  new Cache({engine:'catbox-memory'})
 		done();
 	})
 	describe('cached', function(){
 		before(function( done ){
 			var api = new Api('resource/spec')
-			api.use('cached', new resource({
-					cache: c
-				})
-			);
+			instance = new resource();
+			api.use('cached', instance);
 
+			instance.get_object = sinon.spy( instance.get_object );
 			server.register( [api], done )
 		});
 
@@ -57,11 +61,11 @@ describe('resource', function(){
 					})
 				}
 			], function( ){
-				var _cache = c.client._cache.connection.cache
+				instance.get_object.calledOnce.should.equal( true )
 				Object
-					.keys( _cache  )
+					.keys( instance.cache  )
 					.forEach(function( partition ){
-						Object.keys( _cache[ partition ] ).length.should.be.greaterThan(0)
+						Object.keys( instance.cache[ partition ] ).length.should.be.greaterThan(0)
 					})
 				done();
 			})
